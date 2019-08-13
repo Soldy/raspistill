5e6771a104d47887192adf7a397e90408af5ed80
Soldy <4786022+Soldy@users.noreply.github.com>
2019-08-13 13:50:51 +0100

    main object
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,64 @@
+const spawn = require("child_process").spawn;
+
+exports.raspistill = function(){
+    this.run = "";
+    this.addOptions=function(option){
+        for(let i in option)
+            if(typeof optionsMap[i] !== "undefined")
+                options[
+                    optionsMap[i].toString().toLowerCase()
+                ] = option[i];
+    };
+    this.start=function(stdOutFun, stdErrFun, closeFun, errFun){
+        rpat.run = spawn(
+            "raspistill",
+            getOptions(
+                options
+            )
+        );
+          
+        if(typeof stdOutFun !== "undefined")
+            rpat.run.stdout.on("data", stdOutFun);
+        if(typeof stdErrFun !== "undefined")
+            rpat.run.stderr.on("data", stdErrFun);
+        if(typeof closeFun !== "undefined")
+            rpat.run.on("close", closeFun);
+        if(typeof errFun !== "undefined")
+            rpat.run.on("error", errFun);
+    }; 
+    var rpat    = this,
+        options = {},
+        optionsMap = {
+            "verticalFlip"    : "vf",
+            "horizontalFlip"  : "hf",
+            "rotation"        : "rot",
+            "encoding"        : "e",
+            "query"           : "q",
+            "nopreview"       : "n",
+            "width"           : "w",
+            "height"          : "h",
+            "shutterspeed"    : "ss",
+            "timeout"         : "t",
+            "timelapse"       : "tl",
+            "timelap"         : "tl",
+            "output"          : "o",
+            "o"               : "o",
+            "link"            : "l",
+            "l"               : "l",
+            "contrast"        : "co",
+            "co"              : "co",
+            "brightness"      : "br",
+            "saturation"      : "sa",
+            "iso"             : "ISO",
+            "awb"             : "awb",
+            "awbg"            : "awbg"
+        };
+    var getOptions = function(){
+        let out = ["-v","-n"];
+        for (let i in options){
+            out.push("-"+i.toString());
+            out.push(options[i].toString());
+        }
+        return out;
+    };
+};
